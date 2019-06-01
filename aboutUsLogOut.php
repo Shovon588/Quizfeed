71d316ec51cc97374394320e930eae45888d1b24
Shovon <mainulislam588@gmail.com>
2019-06-02 00:05:41 +0600

    logged out about us page added

---
+++ aboutUsLogOut.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="import" href="https://fonts.googleapis.com/css?family=Exo+2">
+    <title>About Us</title>
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Exo+2');
+    @import url('https://fonts.googleapis.com/css?family=Lobster');
+
+    ul {
+        padding: 6%;
+        font-family: "Exo 2";
+        color: #ffffff;
+    }
+
+    li {
+        font-size: 150%
+    }
+
+    .btn-primary,
+    .btn-primary:hover,
+    .btn-primary:active,
+    .btn-primary:visited {
+        background-color: #000000;
+    }
+</style>
+
+<body style="margin-top:5%">
+    <div class="container">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><i class="fa fa-bars"></i></button>
+            <ul class="dropdown-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="LogOutcalander.php">Calander</a></li>
+                <li><a href="aboutUsLogOut.php">About Us</a></li>
+            </ul>
+            <nobr style="font-family:Lobster; font-size:140%">&nbsp; Quiz Feed</nobr>
+        </div>
+
+
+        <h2 align="center">About Us</h2><br><br>
+        <p style="margin-left:10%;margin-right:10%;text-align;justify">Aiming to lead the country’s booming Digital Financial Service (DFS) revolution, Nagad is a venture by the Bangladesh Post Office that facilitates the day-to-day financial transaction needs of the people.Nagad brand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contributes to the financial inclusivity & socio-economic development of the country.rand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis rand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contrihad mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contrirand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contrihad mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs.</p>
+
+    </div>
+
+</body>
+
+</html>
