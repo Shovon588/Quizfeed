76427b1f83e848a289cc758c73027cf6288ad98a
Shovon <mainulislam588@gmail.com>
2019-06-08 23:36:36 +0600

    good to go with this hopefully

--- readFullBlog.php
+++ readFullBlog.php
@@ -1,109 +1,152 @@
 <?php
-//session_start();
-//require("sessionCheck.php");
+session_start();
+require("sessionCheck.php");
 require("connectToDB.php");
 
 if (isset($_GET['blogID'])) {
     $blogID = $_GET['blogID'];
-    $id = $_SESSION['id'];
 }
 
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Full Blog</title>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-
-        /* Add a gray background color with some padding */
-        body {
-            font-family: Arial;
-            padding: 20px;
-            background: #f1f1f1;
-        }
-
-        /* Add a card effect for articles */
-        .card {
-            background-color: linen;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 
-<body>
-    <?php
-    $query = "SELECT * FROM bose_blog where `blogNo`='$blogID'";
-    $result = mysqli_query($conn, $query);
+<body style="margin:5% 5% 5% 5%">
+    <?php require("header.php"); ?>
 
-
-    while ($row = $result->fetch_assoc()) {
-        $title = $row['blogTitle'];
-        $blog = $row['blogContent'];
-        $time = $row['time'];
-        ?>
+    <br><br><br><br><br>
+    <div class="container-fluid">
         <div class="row">
-            <div class="card" style="width:100%">
-                <h2 align="center"><?php echo "" . $title ?> </h1>
+
+
+            <div class="col-sm-2">
+            </div>
+
+            <div class="col-sm-8" style="background-color:mintcream;border:3px solid black">
+                <?php
+                $query = "SELECT * FROM bose_blog where `blogNo`='$blogID'";
+                $result = mysqli_query($conn, $query);
+
+
+                while ($row = $result->fetch_assoc()) {
+                    $title = $row['blogTitle'];
+                    $blog = $row['blogContent'];
+                    $time = $row['time'];
+                    ?>
+                    <center><a style="font-size:40px;"><?php echo "" . $title ?> </a></center>
                     <h5 align="center" style="color:yellowgreen"><?php echo "Published on: ";
                                                                     echo date("M d,Y h:i A", $time) ?></h5>
-                    <p align="center" style="margin-top:3%"><?php echo "" . $blog ?></p>
+                    <p align="center" style="margin-top:3%;font-size:20px;text-align:justify"><?php echo "" . $blog ?></p>
+                <?php } ?>
+
             </div>
-        <?php } ?>
 
-        <div class="card" style="width:100%;background-color:burlywood;padding-bottom:2%">
-            <h3 align="center">Comments</h3>
-            <?php echo "" . "<a style='font-size:20px' href='http://quizfeed.selisestaging.com/writeComment.php?blogID=$blogID'>Write new comment</a>"; ?>
+            <div class="col-sm-2">
+            </div>
+        </div>
+    </div>
 
-            <?php
-            //fetch comments
 
-            $query = "SELECT * FROM bose_comments where `blogID`='$blogID' order by time desc";
-            $result = mysqli_query($conn, $query);
 
-            if (!$result) { ?>
-                <br><br><br><br><br><br>
-                <div class="card" style="height:max-content;background-color:white;margin-bottom:5%;border:2p x solid black;">
-                    <h1 align="center">No comments right now!!</h1>
+    <div class="container-fluid" style="margin-top:3%">
+        <div class="row">
+
+
+            <div class="col-sm-3">
+            </div>
+
+            <div class="col-sm-6" style="background-color:white;border:3px solid black">
+
+                <center><a style="font-size:40px;">Comments</a></center>
+
+                <a style="font-size:20px" href='#comment' data-toggle='modal'>Write comment</a>
+
+
+                <!-- Write comment modal -->
+                <div id="comment" class="modal">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-body">
+                                <p>
+                                <h1>Ekhane ekta cool editor bosbe</h1>
+                                </p>
+                            </div>
+                            <div class="modal-footer" style="border-top:0px">
+                                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-            <?php
 
-        } else {
-            while ($row = $result->fetch_assoc()) {
-                $userID = $row['userID'];
-                $comment = $row['comment'];
-                $userName = $row['userName'];
+                
+                <br><br>
+                <?php
+                //fetch comments
 
-                $q = "select fullName from bose_user_profile where id=$userID limit 1";
-                $re = mysqli_query($conn, $q);
 
-                if ($re) {
-                    while ($r = $re->fetch_assoc()) {
-                        if ($r['fullName']) {
-                            $userName = $r['fullName'];
-                        }
-                    }
-                }
+                $query = "SELECT * FROM bose_comments where `blogID`='$blogID' order by time desc";
+                $result = mysqli_query($conn, $query);
+                $match  = mysqli_num_rows($result);
 
-                ?>
-                    <br>
-                    <div class="card" style="height:max-content;background-color:white;margin-bottom:2%;border:2px solid black;">
 
-                        <b> <a style="font-size:15px;"><?php echo "By: " . $userName; ?></a><b><br><br />
-                                <p><?php echo "" . $comment; ?> </p>
+                if ($match == 0) { ?>
+                    <br><br><br><br><br><br>
+                    <div style="height:max-content;margin-bottom:20px;padding:15px;border-top:3px solid black">
+                        <h1 align="center">No comments right now!!</h1>
                     </div>
 
                 <?php
+
+            } else {
+                while ($row = $result->fetch_assoc()) {
+                    $userID = $row['userID'];
+                    $comment = $row['comment'];
+                    $userName = $row['userName'];
+                    $time = $row['time'];
+
+                    $q = "select fullName from bose_user_profile where id=$userID limit 1";
+                    $re = mysqli_query($conn, $q);
+
+                    if ($re) {
+                        while ($r = $re->fetch_assoc()) {
+                            if ($r['fullName']) {
+                                $userName = $r['fullName'];
+                            }
+                        }
+                    }
+
+                    ?>
+                        <br>
+                        <div style="height:max-content;margin-bottom:20px;padding:15px;border-top:3px solid black;">
+
+                            <b> <a style="font-size:15px;"><?php echo "By: " . $userName . "<br>At: " . date("M d,Y h:i A", $time); ?></a><b><br><br />
+                                    <p style="font-size:20px"><?php echo "" . $comment; ?> </p>
+                        </div>
+
+                    <?php
+                }
             }
-        }
-        ?>
+            ?>
+
+            </div>
+
+            <div class="col-sm-3">
+            </div>
         </div>
     </div>
+
 </body>
 
 </html>
