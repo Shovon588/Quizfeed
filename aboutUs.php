fc22f6f2edaf07f3d46e2d2bd16a7569de2ca599
Shovon <mainulislam588@gmail.com>
2019-06-09 00:43:25 +0600

    logged in about us added

---
+++ aboutUs.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require("connectToDB.php");
+require("sessionCheck.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+
+<body style="margin:5% 5% 5% 5%;width:auto">
+    <?php include("header.php") ?><br><br><br><br><br><br><br><br><br>
+    <h2 align="center">About Us</h2><br><br>
+    <p style="margin-left:10%;margin-right:10%;text-align;justify">Aiming to lead the country’s booming Digital Financial Service (DFS) revolution, Nagad is a venture by the Bangladesh Post Office that facilitates the day-to-day financial transaction needs of the people.Nagad brand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contributes to the financial inclusivity & socio-economic development of the country.rand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis rand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contrihad mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contrirand is operated by Third Wave Technologies Limited (TWTL).The core philosophy of the service is to fuel possibilities. According to the World Bank’s Global Findex 2017,only 50% of Bangladeshis had mobile banking and/or financial institution accounts as of 2017, of whichs only 21.2% have mobile money accounts. That leaves a large population of unbanked people who are in need of reliable service to assist their financial needs. With a host of essential services such as cash in/out,P2P money transfer and mobile top-ups, the purpose of Nagad is to enhance the lives of people by empowering them with financial flexibility.Nagad sets itself apart by ensuring meticulous customer service. Through its facilities, the company contri</p>
+
+</body>
+
+</html>
