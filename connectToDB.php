2a322fbbb12855b4f6489c649f89c55217ef8b06
Shovon <mainulislam588@gmail.com>
2019-06-08 15:28:31 +0600

    set to selise db

--- connectToDB.php
+++ connectToDB.php
@@ -1,20 +1,20 @@
 <?php 
-  /*
+
 $dbServerName = "intern-sls.cdts6wfxxv6z.eu-central-1.rds.amazonaws.com";
     $dbServerUserID = "slsadmin";
     $dbServerPass = "EMSAFNgw04ljnyKN4";
     $dbName = "bose";
 
     $conn = mysqli_connect( $dbServerName , $dbServerUserID , $dbServerPass , $dbName  );
-*/
-  
     
+
+  /*
     $dbServerName = "localhost";
     $dbServerUserID = "root";
     $dbServerPass = "";
     $dbName = "bose";
 
     $conn = mysqli_connect( $dbServerName , $dbServerUserID , $dbServerPass , $dbName  );
-    
+    */
 
 ?> 
