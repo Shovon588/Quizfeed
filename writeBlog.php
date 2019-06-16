870c0ac84f3f5db5279411b3ad0ee9bd9bf68167
Shovon <mainulislam588@gmail.com>
2019-06-17 03:07:19 +0600

    write blog functionality

---
+++ writeBlog.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Write comment</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <style>
+        ::placeholder {
+            /* Chrome, Firefox, Opera, Safari 10.1+ */
+            color: red;
+            opacity: 1;
+            font-size:20px;
+            padding-left:3%;
+            text-align: center;
+            /* Firefox */
+        }
+    </style>
+
+</head>
+
+<body style="margin:5% 5% 5% 5%">
+
+
+    <?php require("header.php"); ?>
+
+    <br><br><br>
+    <div class="container">
+        <form action="writeBlogServer.php" method="post">
+            <center>
+                <h2>Title</h2>
+                <input style="width:85%;height:50px;border:2px solid black;font-size:20px" type="text" placeholder="Your blog title." name="title" required>
+                <br><br><br><br>
+                <h2>Blog</h2>
+                <textarea class="ckeditor" name="editor" required></textarea><br>
+
+                <button type="submit" style="font-size:20px" class="btn btn-success" name="submit">Post</button>
+            </center>
+        </form>
+
+
+    </div>
+
+
+
+    <script src="ckeditor1/ckeditor.js"></script>
+
+</body>
+
+</html>
---
+++ writeBlogServer.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+//require("sessionCheck.php");
+require("connectToDB.php");
+
+
+
+if (isset($_POST['submit'])) {
+    if (isset($_POST['editor']) && !empty($_POST['editor'])) {
+        $blog = $_POST['editor'];
+        $title=$_POST['title'];
+        $time = time() + 21600;
+        $bloggerID =1;
+        $userName = 'user' . $bloggerID;
+
+        echo "".$blog.' '.$title.' ' .$time;
+    } else {
+        $message = "You did not write any blog.";
+
+        echo "<script type='text/javascript'>alert('$message');
+    window.location.href='writeBlog.php';
+    
+    </script>";
+        echo 'window.location.href = "writeBlog.php";';
+    }
+
+
+    if (isset($blog) && !empty($blog)) {
+        //have to insert data into database
+
+        $query = "INSERT INTO `bose_blog` (`blogTitle`,`blogContent`,`time`,`bloggerID`,`bloggerName`) VALUES('$title','$blog','$time','$bloggerID','$bloggerName')";
+        $result = mysqli_query($conn, $query);
+
+        if (!$result) {
+            die("" . mysqli_error($conn));
+            $message = "Something went wrong.\\nTry again";
+
+            echo "<script type='text/javascript'>alert('$message');
+              window.location.href='writeBlog.php';
+    
+    </script>";
+            echo 'window.location.href = "writeBlog.php";';
+            
+        } else {
+            header("Location: homepage.php");
+        }
+    }
+}
