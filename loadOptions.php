3fdbcfd5f6c376fd8f1db7c3cff07edc2c0e410d
GunsAndBugs <37249607+GunsAndBugs@users.noreply.github.com>
2019-06-17 13:37:24 +0600

    Update loadOptions.php

--- loadOptions.php
+++ loadOptions.php
@@ -8,7 +8,7 @@
     include 'connectToDB.php';
     $Qid = $_POST['Qid'];
 
-    $sql = "SELECT * FROM BOSE_add_option WHERE `problemId` = '$Qid'";
+    $sql = "SELECT * FROM bose_add_option WHERE `problemId` = '$Qid'";
     $result = $conn->query($sql);
     $row = $result->fetch_all(MYSQLI_ASSOC);
     //print_r(json_encode($row));
