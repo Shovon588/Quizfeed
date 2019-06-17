219ff34ffec79ac49c2801ade9e4053ee4629f2d
GunsAndBugs <37249607+GunsAndBugs@users.noreply.github.com>
2019-06-17 13:51:03 +0600

    Update viewProblem.php

--- viewProblem.php
+++ viewProblem.php
@@ -82,7 +82,7 @@
             <div class="col-sm-2"></div>
         </div>
     </div>
-  <a href="questionArchive.php" > Back to QuestionArchive </a>
+  <center><a href="questionArchive.php" > Back to QuestionArchive </a></center>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
